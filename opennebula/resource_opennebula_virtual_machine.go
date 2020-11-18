5e19b13c2fa1aea8706fbde99708527818b85837
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2020-11-18 10:44:03 +0100

    remove partial from VM update method

--- opennebula/resource_opennebula_virtual_machine.go
+++ opennebula/resource_opennebula_virtual_machine.go
@@ -484,9 +484,6 @@ func resourceOpennebulaVirtualMachineExists(d *schema.ResourceData, meta interfa
 
 func resourceOpennebulaVirtualMachineUpdate(d *schema.ResourceData, meta interface{}) error {
 
-	// Enable partial state mode
-	d.Partial(true)
-
 	//Get VM
 	vmc, err := getVirtualMachineController(d, meta)
 	if err != nil {
@@ -508,7 +505,6 @@ func resourceOpennebulaVirtualMachineUpdate(d *schema.ResourceData, meta interfa
 		// TODO: fix it after 5.10 release
 		// Force the "decrypt" bool to false to keep ONE 5.8 behavior
 		vm, err := vmc.Info(false)
-		d.SetPartial("name")
 		log.Printf("[INFO] Successfully updated name (%s) for VM ID %x\n", vm.Name, vm.ID)
 	}
 
@@ -519,7 +515,6 @@ func resourceOpennebulaVirtualMachineUpdate(d *schema.ResourceData, meta interfa
 				return err
 			}
 		}
-		d.SetPartial("permissions")
 		log.Printf("[INFO] Successfully updated Permissions VM %s\n", vm.Name)
 	}
 
@@ -568,7 +563,6 @@ func resourceOpennebulaVirtualMachineUpdate(d *schema.ResourceData, meta interfa
 				return fmt.Errorf("vm disk detach: %s", err)
 
 			}
-			d.SetPartial("disk")
 		}
 
 		// get the list of disks to attach
@@ -588,7 +582,6 @@ func resourceOpennebulaVirtualMachineUpdate(d *schema.ResourceData, meta interfa
 			if err != nil {
 				return fmt.Errorf("vm disk attach: %s", err)
 			}
-			d.SetPartial("disk")
 		}
 	}
 
@@ -616,7 +609,6 @@ func resourceOpennebulaVirtualMachineUpdate(d *schema.ResourceData, meta interfa
 				return fmt.Errorf("vm nic detach: %s", err)
 
 			}
-			d.SetPartial("nic")
 		}
 
 		// get the list of nics to attach
@@ -632,14 +624,8 @@ func resourceOpennebulaVirtualMachineUpdate(d *schema.ResourceData, meta interfa
 			if err != nil {
 				return fmt.Errorf("vm nic attach: %s", err)
 			}
-
-			d.SetPartial("nic")
 		}
 	}
-
-	// We succeeded, disable partial mode. This causes Terraform to save
-	// save all fields again.
-	d.Partial(false)
 
 	return nil
 }
